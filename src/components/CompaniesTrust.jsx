8540f15829a1085550d4b911e8e9a53e776cdd5c
akash-535 <akashbish9e@gmail.com>
2024-12-10 13:53:50 +0530

    overflow auto added on companies icon

--- src/components/CompaniesTrust.jsx
+++ src/components/CompaniesTrust.jsx
@@ -8,7 +8,7 @@ const CompaniesTrust = () => {
             <div className='container px-5 max-xl:pt-14 max-md:pt-10 2xl:pt-14'>
                 <h3 className='xl:hidden 2xl:text-start text-custom-3xl font-normal leading-custom-2xl max-md:text-3xl max-sm:text-2xl max-[374.98px]:text-xl max-w-[548px] mx-auto text-center'>More than <span className='text-pacific-blue font-bold'>80,000</span>+ companies trust bill central </h3>
                 <div
-                    className='pt-16 flex gap-[51px] items-center justify-between max-md:pt-8 max-sm:pt-5 overflow-x-auto'>
+                    className='pt-16 flex gap-[51px] items-center justify-between max-md:pt-8 max-sm:pt-5 max-xl:overflow-x-auto'>
                     {COMPANIES_LOGO_LIST.map((obj, i) => (
                         <div key={i} className='hover:scale-105 duration-300 ease-linear'>
                             <img className={`${i === 0 ? 'max-w-[127px]' : i === 1 ? 'max-w-[102px]' : i === 2 ? 'max-w-[94px] max-h-[32px]' : i === 3 ? 'max-w-[49px] ' : i === 4 ? 'max-w-[62px]' : i === 5 ? 'max-w-[84px]' : i === 6 ? 'max-w-[63px]' : ''}`} src={obj} alt="companies logo" />
